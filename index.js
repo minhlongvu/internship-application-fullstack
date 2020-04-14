7169ed16e8f8e0e6df0d4534b7781b7bee7f2e8d
Albert Vu <44858018+minhlongvu@users.noreply.github.com>
2020-04-14 12:31:57 -0500

    Added updated template files

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond with URL variant A or B
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  try {
+    fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then((response) => {
+      urlArrayJson = response.json();
+      var choose = Math.random();
+      if (choose == 0){
+        // windows.location.replace(urlArrayJson[0]);
+        return new Response(urlArrayJson[0]);
+      }
+      else {
+        // windows.location.replace(urlArrayJson[1]);
+        return new Response(urlArrayJson[1]);
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+  } catch (error) {
+    return new Response(error.stack || error);
+  }
 }
